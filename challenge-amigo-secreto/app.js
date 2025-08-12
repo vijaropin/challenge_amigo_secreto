028b2cd043be96b0dbee20510be62a789f287dc8
Javier Rodriguez <vijaro-4@hotmail.com>
2025-08-12 16:26:45 -0500

    Creacion variable

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -1,4 +1,4 @@
-//variables
-// encontrar las variaNLES
-// 
+// 1. Crear una lista vacía para guardar los nombres de los amigos.
+// // Usaremos un "let" porque el contenido de la lista va a cambiar.
+let amigos = [];
 
